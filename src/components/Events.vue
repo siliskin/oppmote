14f29376373a630aa349bb7407097e0941415e1a
Jan Arne Molund <siliskin@gmail.com>
2018-12-11 16:44:04 +0100

    Added julestemning

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -1,8 +1,46 @@
 <template>
-  <section class="hero">
-    <div id="ground"></div>
+  <section class="hero is-fullheight bkg">
     <div class="section">
-      <div class="container">
+      <img src="../assets/christmas.png"/>
+      <div class="snowflakes" aria-hidden="true">
+        <div class="snowflake">
+          ❅
+        </div>
+        <div class="snowflake">
+          ❆
+        </div>
+        <div class="snowflake">
+          ❅
+        </div>
+        <div class="snowflake">
+          ❆
+        </div>
+        <div class="snowflake">
+          ❅
+        </div>
+        <div class="snowflake">
+          ❆
+        </div>
+        <div class="snowflake">
+          ❅
+        </div>
+        <div class="snowflake">
+          ❆
+        </div>
+        <div class="snowflake">
+          ❅
+        </div>
+        <div class="snowflake">
+          ❆
+        </div>
+        <div class="snowflake">
+          ❅
+        </div>
+        <div class="snowflake">
+          ❆
+        </div>
+      </div>
+      <div class="container has-text-white">
         <div class="column is-6 is-offset-3">
           <div v-if="this.loading">
             <div class="spinner">
@@ -13,12 +51,12 @@
               <div class="rect5"></div>
             </div>
           </div>
-          <h1 class="title">
+          <h1 class="title" style="color:white">
             {{ nextEvent.name }}
           </h1>
           <div v-if="!this.loading">
-            <h2 class="subtitle">
-              {{ nextEvent.location }} <strong>{{nextEvent.date}}</strong>
+            <h2 class="subtitle"  style="color:white">
+              {{ nextEvent.location }} <strong  style="color:white">{{nextEvent.date}}</strong>
             </h2>
             <h3>
               <strong v-if="nextEvent.participants.length > 4 ">{{nextEvent.participants.length}}</strong>
@@ -130,7 +168,7 @@ export default {
           { 
             data: perps,
             fill: false,
-            borderColor: '#36495d',
+            borderColor: '#fff',
             borderWidth: 3
           },
           ]
@@ -295,7 +333,18 @@ methods: {
     will-change: transform;
   }
 
-  .bkkk {
-   background:#FFF;
+  .bkg {
+   background: url("../assets/background.png");
  }
- </style>
+ /* customizable snowflake styling */
+ .snowflake {
+  color: #fff;
+  font-size: 1em;
+  font-family: Arial, sans-serif;
+  text-shadow: 0 0 5px #000;
+}
+
+@-webkit-keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@-webkit-keyframes snowflakes-shake{0%,100%{-webkit-transform:translateX(0);transform:translateX(0)}50%{-webkit-transform:translateX(80px);transform:translateX(80px)}}@keyframes snowflakes-fall{0%{top:-10%}100%{top:100%}}@keyframes snowflakes-shake{0%,100%{transform:translateX(0)}50%{transform:translateX(80px)}}.snowflake{position:fixed;top:-10%;z-index:9999;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;cursor:default;-webkit-animation-name:snowflakes-fall,snowflakes-shake;-webkit-animation-duration:10s,3s;-webkit-animation-timing-function:linear,ease-in-out;-webkit-animation-iteration-count:infinite,infinite;-webkit-animation-play-state:running,running;animation-name:snowflakes-fall,snowflakes-shake;animation-duration:10s,3s;animation-timing-function:linear,ease-in-out;animation-iteration-count:infinite,infinite;animation-play-state:running,running}.snowflake:nth-of-type(0){left:1%;-webkit-animation-delay:0s,0s;animation-delay:0s,0s}.snowflake:nth-of-type(1){left:10%;-webkit-animation-delay:1s,1s;animation-delay:1s,1s}.snowflake:nth-of-type(2){left:20%;-webkit-animation-delay:6s,.5s;animation-delay:6s,.5s}.snowflake:nth-of-type(3){left:30%;-webkit-animation-delay:4s,2s;animation-delay:4s,2s}.snowflake:nth-of-type(4){left:40%;-webkit-animation-delay:2s,2s;animation-delay:2s,2s}.snowflake:nth-of-type(5){left:50%;-webkit-animation-delay:8s,3s;animation-delay:8s,3s}.snowflake:nth-of-type(6){left:60%;-webkit-animation-delay:6s,2s;animation-delay:6s,2s}.snowflake:nth-of-type(7){left:70%;-webkit-animation-delay:2.5s,1s;animation-delay:2.5s,1s}.snowflake:nth-of-type(8){left:80%;-webkit-animation-delay:1s,0s;animation-delay:1s,0s}.snowflake:nth-of-type(9){left:90%;-webkit-animation-delay:3s,1.5s;animation-delay:3s,1.5s}.snowflake:nth-of-type(10){left:25%;-webkit-animation-delay:2s,0s;animation-delay:2s,0s}.snowflake:nth-of-type(11){left:65%;-webkit-animation-delay:4s,2.5s;animation-delay:4s,2.5s}
+</style>
+
+</style>
